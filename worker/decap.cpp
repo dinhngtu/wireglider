55d3dc22be452d9300232f4c1b4b9a845423f2d3
Tu Dinh Ngoc <dinhngoc.tu@irit.fr>
2024-07-14 00:32:24 +0000

    opb decap is broken anyway, remove it for now

--- worker/decap.cpp
+++ worker/decap.cpp
@@ -46,49 +46,32 @@ void Worker::do_server(epoll_event *ev) {
 
             // recvbatch.pbeps is already set to the correct size
             for (auto &[pb, ep] : recvbatch.pbeps) {
-                if constexpr (use_opb_mainloop) {
-                    auto batch = do_server_decap(pb, ep, decapbuf);
-                    if (!batch)
-                        continue;
-
-                    auto sendlist = new ServerSendList(std::move(batch->retpkt), ep);
-                    auto ret = sendlist->send(_arg.server->fd());
-                    if (ret)
-                        delete sendlist;
-                    else
-                        _serversend.push_back(*sendlist);
-
-                    if (!worker_impl::do_tun_write_batch(_arg.tun->fd(), *batch))
-                        do_tun_requeue_batch(*batch);
-
+                auto batch = do_server_decap_ref(pb, ep, decapbuf);
+                if (!batch) {
+                    DBG_PRINT("decap failed\n");
+                    continue;
                 } else {
-                    auto batch = do_server_decap_ref(pb, ep, decapbuf);
-                    if (!batch) {
-                        DBG_PRINT("decap failed\n");
-                        continue;
-                    } else {
-                        DBG_PRINT(
-                            "got decap batch size v4 {} {} v6 {} {} unrel/retpkt {} {}\n",
-                            batch->tcp4.size(),
-                            batch->udp4.size(),
-                            batch->tcp6.size(),
-                            batch->udp6.size(),
-                            batch->unrel.size(),
-                            batch->retpkt.size());
-                    }
-
-                    auto ret = server_send_reflist(batch->retpkt, ep);
-                    if (ret.has_value()) {
-                        auto tosend = new ServerSendList(ep);
-                        for (auto &iov : ret.value())
-                            tosend->push_back(iov);
-                        tosend->finalize();
-                        _serversend.push_back(*tosend);
-                    }
-
-                    if (!worker_impl::do_tun_write_batch(_arg.tun->fd(), *batch))
-                        do_tun_requeue_batch(*batch);
+                    DBG_PRINT(
+                        "got decap batch size v4 {} {} v6 {} {} unrel/retpkt {} {}\n",
+                        batch->tcp4.size(),
+                        batch->udp4.size(),
+                        batch->tcp6.size(),
+                        batch->udp6.size(),
+                        batch->unrel.size(),
+                        batch->retpkt.size());
                 }
+
+                auto ret = server_send_reflist(batch->retpkt, ep);
+                if (ret.has_value()) {
+                    auto tosend = new ServerSendList(ep);
+                    for (auto &iov : ret.value())
+                        tosend->push_back(iov);
+                    tosend->finalize();
+                    _serversend.push_back(*tosend);
+                }
+
+                if (!worker_impl::do_tun_write_batch(_arg.tun->fd(), *batch))
+                    do_tun_requeue_batch(*batch);
             }
         }
     }
