9274f4c3529e2acf198ad943fd85ef4cce49c8af
Tu Dinh Ngoc <dinhngoc.tu@irit.fr>
2024-09-26 14:12:03 +0000

    configure.sh: echo configure ok

--- configure.sh
+++ configure.sh
@@ -9,3 +9,5 @@ ip link set wg0 mtu 1964
 #wg set wg0 peer XXmnRm3crIM5cU92d1GA7l5sKzU+wosKfeWAYq1edCY= endpoint 192.168.0.3:51820 allowed-ips 10.77.44.2/32
 wg set wg0 peer XXmnRm3crIM5cU92d1GA7l5sKzU+wosKfeWAYq1edCY= endpoint 10.88.77.3:51820 allowed-ips 10.77.44.2/32
 #wg set wg0 peer 0kZFZAJSwoY5iVCE910ILzNQBBPo5WyewKcD+LnzZGs= endpoint 192.168.88.177:51820 allowed-ips 10.77.44.99/32
+
+echo Configure OK
